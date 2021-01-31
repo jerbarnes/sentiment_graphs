bc29c80c82e4bdf0c07c2209f9eed723519d7156
Robin Kurtz <robin.kurtz@kb.se>
2021-01-31 19:49:27 +0100

    call bootstrap on the best and second best pairs

---
+++ analysis/do_bootstrap.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+lang=norec;
+a=head_final-inside_label-dep_edges
+b=head_final
+python bootstrap.py ../data/sent_graphs/$lang/ ../experiments/$lang/ $a $b;
+
+lang=eu;
+a=head_final-inside_label-dep_edges
+b=head_final-inside_label-dep_edges-dep_labels
+python bootstrap.py ../data/sent_graphs/$lang/ ../experiments/$lang/ $a $b;
+
+lang=ca;
+a=head_first
+b=head_first-inside_label
+python bootstrap.py ../data/sent_graphs/$lang/ ../experiments/$lang/ $a $b;
+
+lang=mpqa;
+a=head_final
+b=head_final-inside_label
+python bootstrap.py ../data/sent_graphs/$lang/ ../experiments/$lang/ $a $b;
+
+lang=ds_unis;
+a=head_final-inside_label-dep_edges
+b=head_final
+python bootstrap.py ../data/sent_graphs/$lang/ ../experiments/$lang/ $a $b;
